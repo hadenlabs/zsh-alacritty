510c9201a39ae55bc54637bae6211a3d625da9aa
@slovacus <slovacus@gmail.com>
2020-12-07 01:29:03 -0500

    refactor: load pkg core internal (#12)

--- internal/main.zsh
+++ internal/main.zsh
@@ -3,15 +3,15 @@
 
 function alacritty::internal::main::factory {
     # shellcheck source=/dev/null
-    source "${ZSH_ALACRITTY_SRC_PATH}"/internal/base.zsh
+    source "${ZSH_ALACRITTY_PATH}"/internal/base.zsh
     case "${OSTYPE}" in
     darwin*)
         # shellcheck source=/dev/null
-        source "${ZSH_ALACRITTY_SRC_PATH}"/internal/osx.zsh
+        source "${ZSH_ALACRITTY_PATH}"/internal/osx.zsh
         ;;
     linux*)
         # shellcheck source=/dev/null
-        source "${ZSH_ALACRITTY_SRC_PATH}"/internal/linux.zsh
+        source "${ZSH_ALACRITTY_PATH}"/internal/linux.zsh
       ;;
     esac
 }
--- zsh-alacritty.zsh
+++ zsh-alacritty.zsh
@@ -8,16 +8,16 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
-ZSH_ALACRITTY_SRC_PATH=$(dirname "${0}")
+ZSH_ALACRITTY_PATH=$(dirname "${0}")
 
 # shellcheck source=/dev/null
-source "${ZSH_ALACRITTY_SRC_PATH}"/config/main.zsh
+source "${ZSH_ALACRITTY_PATH}"/config/main.zsh
 
 # shellcheck source=/dev/null
-source "${ZSH_ALACRITTY_SRC_PATH}"/core/main.zsh
+source "${ZSH_ALACRITTY_PATH}"/core/main.zsh
 
 # shellcheck source=/dev/null
-source "${ZSH_ALACRITTY_SRC_PATH}"/internal/main.zsh
+source "${ZSH_ALACRITTY_PATH}"/internal/main.zsh
 
 # shellcheck source=/dev/null
-source "${ZSH_ALACRITTY_SRC_PATH}"/pkg/main.zsh
+source "${ZSH_ALACRITTY_PATH}"/pkg/main.zsh
